408dd5f0172c81e44ed6d112d2421120607c9bde
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-25 16:13:33 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import { useState } from "react";
 
 function App() {
+  const [state, setState] = useState(false);
+
+  function toggleButton() {
+    const toggle = document.querySelector(".toggle");
+    const body = document.querySelector(".body");
+    const circle = document.querySelector(".circle");
+    toggle.addEventListener("click", () => {
+      if (toggle.classList.contains("nothing")) {
+        body.classList.replace("nothing", "forward");
+        toggle.classList.replace("nothing", "forward");
+        circle.classList.replace("nothing", "forward");
+      } else if (toggle.classList.contains("reverse")) {
+        body.classList.replace("reverse", "forward");
+        toggle.classList.replace("reverse", "forward");
+        circle.classList.replace("reverse", "forward");
+      } else if (toggle.classList.contains("forward")) {
+        body.classList.replace("forward", "reverse");
+        toggle.classList.replace("forward", "reverse");
+        circle.classList.replace("forward", "reverse");
+      }
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body nothing">
+      <div className="toggle nothing" onClick={toggleButton}>
+        <span className="circle nothing"></span>
+      </div>
+      <h4 class="text">I Love You</h4>
     </div>
   );
 }

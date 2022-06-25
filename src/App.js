7410c62b139856ac64090639848d90ac7b3badc0
zxcwer <61655281+zxcwer@users.noreply.github.com>
2022-06-25 16:33:47 +0900

    updated toggleButton using useState

--- src/App.js
+++ src/App.js
@@ -2,32 +2,22 @@ import "./style.css";
 import { useState } from "react";
 
 function App() {
-  const [state, setState] = useState(false);
+  const [state, setState] = useState("");
 
   function toggleButton() {
-    const toggle = document.querySelector(".toggle");
-    const body = document.querySelector(".body");
-    const circle = document.querySelector(".circle");
-    toggle.addEventListener("click", () => {
-      if (toggle.classList.contains("nothing")) {
-        body.classList.replace("nothing", "forward");
-        toggle.classList.replace("nothing", "forward");
-        circle.classList.replace("nothing", "forward");
-      } else if (toggle.classList.contains("reverse")) {
-        body.classList.replace("reverse", "forward");
-        toggle.classList.replace("reverse", "forward");
-        circle.classList.replace("reverse", "forward");
-      } else if (toggle.classList.contains("forward")) {
-        body.classList.replace("forward", "reverse");
-        toggle.classList.replace("forward", "reverse");
-        circle.classList.replace("forward", "reverse");
-      }
-    });
+    if (state == "") {
+      setState(" forward");
+    } else if (state == " forward") {
+      setState(" reverse");
+    } else if (state == " reverse") {
+      setState(" forward");
+    }
   }
+
   return (
-    <div className="body nothing">
-      <div className="toggle nothing" onClick={toggleButton}>
-        <span className="circle nothing"></span>
+    <div className={`body${state}`}>
+      <div className={`toggle${state}`} onClick={toggleButton}>
+        <span className={`circle${state}`}></span>
       </div>
       <h4 class="text">I Love You</h4>
     </div>
